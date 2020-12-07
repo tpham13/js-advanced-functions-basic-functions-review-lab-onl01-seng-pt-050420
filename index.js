02c65cd72790ecde2f352458c04276d8baca9362
tpham13 <tpham0123@gmail.com>
2020-12-07 14:24:05 -0800

    commit tree 63833bc2a9b8c00851e506ea82f10b6858cec4fa

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function saturdayFun(activity= "roller-skate"){
+   return`This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+  }
+
+function wrapAdjective(outerArg="*"){
+    return function(innerArg="special"){
+        return `You are ${outerArg}${innerArg}${outerArg}!`
+    }
+}
+
+let Calculator = {
+    add: function(a,b){
+        return a + b;
+    }, 
+    subtract: function(a,b){
+        return a-b
+    }, 
+    multiply: function(a,b){
+        return a * b
+    }, 
+    divide: function(a,b){
+        return a / b
+    },
+}
+
+function actionApplyer(start, ray){
+    let a = start 
+    for(let i = 0; i < ray.length; i++){
+        a = ray[i](a)
+    }
+    return a 
+
+}
